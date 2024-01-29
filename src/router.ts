174db95fed9bacc58d11b768959e96af2a41d886
Marika Di Girolamo <marika.digirolamo@bitbull.it>
2024-01-29 19:21:53 +0100

    Update

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { body, validationResult } from 'express-validator';
 import { handleInputErrors } from './modules/middleware';
 import { createManyProducts, addProduct, deleteProduct, getOneProduct, getProducts, updateProduct } from './handlers/product';
+import { getUpdates } from './handlers/update';
 
 const router = Router()
 
@@ -12,19 +13,28 @@ const router = Router()
 
 router.get('/products', getProducts)
 router.get('/product/:id', () => {}) //:id è un parametro del router, possiamo impostare quello che vogliamo
-router.put('/product/:id', body('name').isString(), handleInputErrors, updateProduct);//handleInputErrors è una callback 
-router.put('/product/:id', body('name').isString(), handleInputErrors, deleteProduct);//handleInputErrors è una callback 
+router.put('/product/:id', 
+body('name').isString(), handleInputErrors, updateProduct);//handleInputErrors è una callback 
+
+router.put('/product/:id', 
+body('name').isString(), handleInputErrors, deleteProduct);//handleInputErrors è una callback 
 
 
-router.post('/product', body('name').isString(),body('description').optional().isString(), handleInputErrors, addProduct);
-router.post('/products', body('products').isArray(),body('products.*.name').isString(), handleInputErrors, createManyProducts);
+router.post('/product', 
+body('name').isString(),
+body('description').optional().isString(), handleInputErrors, addProduct);
+
+router.post('/products', 
+body('products').isArray(),
+body('products.*.name').isString(), handleInputErrors, createManyProducts);
+
 router.delete('/product/:id', handleInputErrors, deleteProduct); 
 
 /**
  * Update Product Router
  */
 
-router.get('/updates', () => {})
+router.get('/updates', getUpdates)
 router.get('/update/:id', () => {}) //:id è un parametro del router, possiamo impostare quello che vogliamo
 router.put('/update/:id', 
     body('title').optional(), 
